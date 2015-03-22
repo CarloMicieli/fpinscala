c7d9f04f9d6051da75eb1492da59e949be0429e9
CarloMicieli <carlo.micieli@gmail.com>
2015-03-22 16:02:43 +0100

    Added exercises E4.06 and E4.07

--- src/main/scala/fpinscala/exercises/chapter04/Es4_06.scala
+++ src/main/scala/fpinscala/exercises/chapter04/Es4_06.scala
@@ -1,8 +1,9 @@
 package fpinscala.exercises.chapter04
 
 /**
- * EXERCISE 4.6] Implement versions of map, flatMap, orElse, and map2 on Either that operate on the
- *               Right value.
+ * EXERCISE 4.6] Implement versions of `map`, `flatMap`, `orElse`, and `map2` on
+ *               `Either` that operate on the `Right` value.
  */
 object Es4_06 {
+  // see Either.scala
 }
--- src/test/scala/fpinscala/exercises/chapter04/Es4_07Suite.scala
+++ src/test/scala/fpinscala/exercises/chapter04/Es4_07Suite.scala
@@ -1,10 +1,19 @@
 package fpinscala.exercises.chapter04
 
-import org.scalatest.FunSuite
+import org.scalatest.{Matchers, FunSuite}
 import Es4_07._
 
-class Es4_07Suite extends FunSuite {
-  test("not_implemented_yet") {
-    assert(1 == 1)
+class Es4_07Suite extends FunSuite with Matchers {
+  test("it should sequence a list of Either values") {
+    val l1: List[Either[String, Int]] = List(Right(1), Right(2), Right(3))
+    val l2: List[Either[String, Int]] = List(Right(1), Left("first error"), Right(3), Left("second error"))
+    sequence(l1) should be(Right(List(1, 2, 3)))
+    sequence(l2) should be(Left("first error"))
+  }
+
+  test("it should traverse a list of Either values") {
+    val f: Int => Either[String, Int] = x => if (x > 0) Right(x) else Left("Negative")
+    traverse(List(1, 2, 3))(f) should be(Right(List(1, 2, 3)))
+    traverse(List(1, -2, 3))(f) should be(Left("Negative"))
   }
 }
