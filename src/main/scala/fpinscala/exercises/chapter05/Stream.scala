d1d9ce3739695c1d2bd8d1a35a3d4a1e69abbab9
CarloMicieli <carlo.micieli@gmail.com>
2015-03-31 21:28:46 +0200

    Added exercises E5.1

--- src/main/scala/fpinscala/exercises/chapter05/Stream.scala
+++ src/main/scala/fpinscala/exercises/chapter05/Stream.scala
@@ -1,6 +1,17 @@
 package fpinscala.exercises.chapter05
 
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+
+  def toList: List[A] = this match {
+    case Empty => List.empty[A]
+    case Cons(h, t) => h() :: t().toList
+  }
+}
+
 object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
@@ -10,12 +21,12 @@ object Stream {
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
+
   def empty[A]: Stream[A] = Empty
-  def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
-/*
-  def headOption[A]: Option[A] = this match {
-    case Empty => None
-    case Cons(h, t) => Some(h())
-  }
-*/
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty)
+      empty
+    else
+      cons(as.head, apply(as.tail: _*))
 }
--- src/test/scala/fpinscala/exercises/chapter05/Es5_01Suite.scala
+++ src/test/scala/fpinscala/exercises/chapter05/Es5_01Suite.scala
@@ -4,7 +4,9 @@ import org.scalatest.FunSuite
 import Es5_01._
 
 class Es5_01Suite extends FunSuite {
-  test("not_implemented_yet") {
-    assert(1 == 1)
+  test("it should convert a Stream to lists") {
+    assert(streamToList() == List(1, 2, 3))
+    assert(Stream.empty[Int].toList == List())
+    assert(Stream("a", "b", "c").toList == List("a", "b", "c"))
   }
 }
