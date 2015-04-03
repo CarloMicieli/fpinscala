23b60998b26e67f5420fa61fc9bd9ffe5aa58d02
CarloMicieli <carlo.micieli@gmail.com>
2015-04-03 16:43:12 +0200

    Initial commit for exercise 5.13

--- src/main/scala/fpinscala/exercises/chapter05/Es5_13.scala
+++ src/main/scala/fpinscala/exercises/chapter05/Es5_13.scala
@@ -1,11 +1,19 @@
 package fpinscala.exercises.chapter05
 
 /**
- * EXERCISE 5.13] Use unfold to implement map, take, takeWhile, zipWith (as in chapter 3), and
- *                zipAll. The zipAll function should continue the traversal as long as either stream
- *                has more elements—it uses Option to indicate whether each stream has been
+ * EXERCISE 5.13] Use `unfold` to implement `map`, `take`, `takeWhile`, `zipWith` (as in chapter 3), and
+ *                `zipAll`. The `zipAll` function should continue the traversal as long as either stream
+ *                has more elements—it uses `Option` to indicate whether each stream has been
  *                exhausted.
  */
 object Es5_13 {
-  def zipAll[A,B](s2: Stream[B]): Stream[(Option[A],Option[B])] = ???
+  def map[A, B](st: Stream[A])(f: A => B): Stream[B] = ???
+
+  def take[A](st: Stream[A])(n: Int): Stream[A] = ???
+
+  def takeWhile[A](st: Stream[A])(p: A => Boolean): Stream[A] = ???
+
+  def zipWith[A, B](as: Stream[A], bs: Stream[A])(f: (A, A) => B): Stream[B] = ???
+
+  def zipAll[A, B](s2: Stream[B]): Stream[(Option[A], Option[B])] = ???
 }
--- src/test/scala/fpinscala/exercises/chapter05/Es5_13Suite.scala
+++ src/test/scala/fpinscala/exercises/chapter05/Es5_13Suite.scala
@@ -4,7 +4,8 @@ import org.scalatest.FunSuite
 import Es5_13._
 
 class Es5_13Suite extends FunSuite {
-  test("not_implemented_yet") {
-    assert(1 == 1)
+  ignore("it should implement map in terms of unfold") {
+    val s = Stream.range(1, 5)
+    assert(map(s)(_ * 2).toList == List())
   }
 }
