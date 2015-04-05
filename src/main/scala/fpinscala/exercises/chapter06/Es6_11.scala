eb2f024eb7ffe6b6c6b2b4ac4faa6a8ba8c95aaa
CarloMicieli <carlo.micieli@gmail.com>
2015-04-05 17:17:24 +0200

    Added exercise Es6.11

--- src/main/scala/fpinscala/exercises/chapter06/Es6_11.scala
+++ src/main/scala/fpinscala/exercises/chapter06/Es6_11.scala
@@ -1,7 +1,7 @@
 package fpinscala.exercises.chapter06
 
 /**
- * EXERCISE 6.11] Hard: To gain experience with the use of State, implement a finite state automaton
+ * EXERCISE 6.11] Hard: To gain experience with the use of `State`, implement a finite state automaton
  *                that models a simple candy dispenser. The machine has two types of input: you can
  *                insert a coin, or you can turn the knob to dispense candy. It can be in one of two
  *                states: locked or unlocked. It also tracks how many candies are left and how many
@@ -11,9 +11,37 @@ object Es6_11 {
   sealed trait Input
   case object Coin extends Input
   case object Turn extends Input
-  case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-  trait State[A, B]
+  case class Machine(locked: Boolean, candies: Int, coins: Int) {
+    val outOfCandies = candies == 0
+  }
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  type State[S, +A] = S => (A, S)
+  type MchState = State[Machine, (Int, Int)]
+
+  private def run(inp: Input): MchState = inp match {
+    case Coin => addCoin
+    case Turn => turnKnob
+  }
+
+  private val doNothing: MchState = mch => ((mch.coins, mch.candies), mch)
+
+  private val addCoin: MchState = {
+    case mch@Machine(true, ca, co) if !mch.outOfCandies => ((co + 1, ca), Machine(false, ca, co + 1))
+    case mch => doNothing(mch)
+  }
+
+  private val turnKnob: MchState = {
+    case mch@Machine(false, ca, co) if !mch.outOfCandies => ((co, ca - 1), Machine(true, ca - 1, co))
+    case mch => doNothing(mch)
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = inputs match {
+    case List() => doNothing
+    case i :: is =>
+      mch => {
+        val (_, m) = run(i)(mch)
+        simulateMachine(is)(m)
+      }
+  }
 }
--- src/test/scala/fpinscala/exercises/chapter06/Es6_11Suite.scala
+++ src/test/scala/fpinscala/exercises/chapter06/Es6_11Suite.scala
@@ -4,7 +4,31 @@ import org.scalatest.FunSuite
 import Es6_11._
 
 class Es6_11Suite extends FunSuite {
-  test("not_implemented_yet") {
-    assert(1 == 1)
+  test("it should simulate a candy machine") {
+    val mch = Machine(true, 5, 10)
+    val inputs = List(Coin, Turn, Coin, Turn, Coin, Turn, Coin, Turn)
+
+    val (a, mch2) = simulateMachine(inputs)(mch)
+
+    assert(a == (14, 1))
+    assert(mch2.candies == 1)
+    assert(mch2.coins == 14)
+  }
+
+  test("it should do nothing if there is no candies") {
+    val mch = Machine(true, 0, 10)
+    val inputs = List(Coin, Turn)
+
+    val (a, mch2) = simulateMachine(inputs)(mch)
+    assert(a == (10, 0))
+    assert(mch == mch2)
+  }
+
+  test("it should do nothing if the machine is already unlocked") {
+    val mch = Machine(false, 0, 10)
+    val inputs = List(Turn)
+
+    val (_, mch2) = simulateMachine(inputs)(mch)
+    assert(mch == mch2)
   }
 }
