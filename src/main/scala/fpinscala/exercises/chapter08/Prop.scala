7e7b5b088b9171c8087b1cea013a1d796da06fe5
CarloMicieli <carlo.micieli@gmail.com>
2015-05-10 17:32:36 +0200

    Added exercise 8.03

--- src/main/scala/fpinscala/exercises/chapter08/Prop.scala
+++ src/main/scala/fpinscala/exercises/chapter08/Prop.scala
@@ -1,12 +1,20 @@
 package fpinscala.exercises.chapter08
 
 trait Prop {
-  def check: Boolean
+  import Prop._
+
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
   def &&(that: Prop): Prop = new Prop {
-    def check = this.check && that.check
+    def check = for {
+      x <- this.check.right
+      y <- that.check.right
+    } yield x + y
   }
 }
 
 object Prop {
+  type SuccessCount = Int
+  type FailedCase = String
+
   def forAll[A](a: Gen[A])(p: A => Boolean): Prop = ???
 }
--- src/test/scala/fpinscala/exercises/chapter08/Es8_03Suite.scala
+++ src/test/scala/fpinscala/exercises/chapter08/Es8_03Suite.scala
@@ -5,18 +5,18 @@ import org.scalatest.{Matchers, FunSuite}
 object Es8_03Suite extends FunSuite with Matchers {
   test("it should implement &&") {
     val success = new Prop {
-      def check = true
+      def check = Right(50)
     }
     val failure = new Prop {
-      def check = false
+      def check = Left(("error", 42))
     }
 
     val p1 = success && success
-    p1.check should be(true)
+    p1.check should be(Right(100))
 
     val p2 = success && failure
     val p3 = failure && failure
-    p2.check should be(false)
-    p3.check should be(false)
+    p2.check should be(Left(("error", 42)))
+    p3.check should be(Left(("error", 42)))
   }
 }
