30b89f28d951a538e2e79deba1aba4d06d1fc7eb
CarloMicieli <carlo.micieli@gmail.com>
2016-06-01 21:44:23 +0200

    Updated traverse

--- src/main/scala/io/github/carlomicieli/fpinscala/chapter04/Es4_05.scala
+++ src/main/scala/io/github/carlomicieli/fpinscala/chapter04/Es4_05.scala
@@ -15,12 +15,19 @@
 // limitations under the License.
 package io.github.carlomicieli.fpinscala.chapter04
 
+/**
+  * EXERCISE 4.5] Implement this function. It’s straightforward to do using `map` and `sequence`, but try
+  * for a more efficient implementation that only looks at the list once. In fact, implement
+  * `sequence` in terms of `traverse`.
+  */
 trait Es4_05 {
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(x => x)
+  def sequence[A](xs: List[Option[A]]): Option[List[A]] = {
+    traverse(xs)(identity)
+  }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+  def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] = {
     val step = mapOpt(f) _
-    a.foldRight(Option.just(List.empty[B]))(step)
+    as.foldRight(Option.just(List.empty[B]))(step)
   }
 
   private def mapOpt[A, B](f: A => Option[B])(x: A, acc: Option[List[B]]): Option[List[B]] = {
